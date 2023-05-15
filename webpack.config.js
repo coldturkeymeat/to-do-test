74d8b01dad0cf7cc504124bc2cec6a4212d74313
Nathan <nathanchee95@gmail.com>
2023-05-15 22:25:08 +1000

    Cleaned and reset to minimal approach

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.html',
+    },
+    output:{
+        filename: 'main.js',
+        path: path.resolve(__dirname,'dist'),
+    }
+}
